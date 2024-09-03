db69df99b39de2e5b6da124dacd8cd28758c669f
Nikita Evseev <nekt7777@gmail.com>
2024-09-03 03:15:15 +0000

    API Gateway fix

--- API Gateway/main.go
+++ API Gateway/main.go
@@ -7,13 +7,10 @@ import (
 )
 
 func main() {
-    // Загрузка конфигурации
     config.LoadConfig()
 
-    // Настройка роутов
     router := routes.SetupRouter()
 
-    // Запуск сервера
     if err := router.Run(":8080"); err != nil {
         log.Fatalf("Failed to run server: %v", err)
     }
