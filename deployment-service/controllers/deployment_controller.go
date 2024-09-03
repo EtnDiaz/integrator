e79cb2018a40f25fea6c03f01d59a4c742ffdf18
Nikita Evseev <nekt7777@gmail.com>
2024-09-03 03:23:14 +0000

    fix ci 3

--- deployment-service/controllers/deployment_controller.go
+++ deployment-service/controllers/deployment_controller.go
@@ -14,7 +14,7 @@ func (ctrl DeploymentController) Deploy(c *gin.Context) {
     releaseName := c.PostForm("releaseName")
     chartPath := c.PostForm("chartPath")
 
-    // todo helm integration for deployment 
+    // todo helm integration for deployment.
     log.Printf("Deploying release: %s using chart: %s", releaseName, chartPath)
 
     c.JSON(http.StatusOK, utils.Response{Message: "Deployment started", Data: releaseName})
